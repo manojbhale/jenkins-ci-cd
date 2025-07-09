5e9279a0d1368b3f0e2c104a1e8277fac996a948
ManojBhale <manoj.bhale.05@gmail.com>
2025-07-09 11:23:50 +0530

    add path variable

--- src/main/java/com/jenkins/controller/WelcomeController.java
+++ src/main/java/com/jenkins/controller/WelcomeController.java
@@ -1,13 +1,14 @@
 package com.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/welcome/{name}")
-    public String welcome(String name) {
+    public String welcome(@PathVariable String name) {
         return "Welcome "+name+" to Jenkins";
     }
 
